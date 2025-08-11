71de28cf697d1bd57d075af601550fb0a7e50af2
kanav789 <kanavrana91@gmail.com>
2025-08-11 08:43:59 +0530

    changes

--- src/Pages/createProfile.tsx
+++ src/Pages/createProfile.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import { getData, PostData } from "../utility/CustomFetchData/CustomFetchData";
 
 const initialData = {
